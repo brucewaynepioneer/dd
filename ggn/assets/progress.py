a19cd632f9d0927e3fd4ea358d3d6d17e5b9fdda
aajcodekrnahai <166581123+aajcodekrnahai@users.noreply.github.com>
2024-09-19 00:18:39 +0530

    Update progress.py

--- ggn/assets/progress.py
+++ ggn/assets/progress.py
@@ -1,3 +1,4 @@
+
 import math
 import os
 import time
@@ -5,10 +6,17 @@ import json
 from ggn.assets.functions import TimeFormatter, humanbytes
 
 #------
-FINISHED_PROGRESS_STR = "🟢"
-UN_FINISHED_PROGRESS_STR = "🔴"
+FINISHED_PROGRESS_STR = "●"
+UN_FINISHED_PROGRESS_STR = "○"
 DOWNLOAD_LOCATION = "/app"
 
+PROGRESS_BAR = """`\n╭─── ✪ Progress ✪
+├ ⚡ [{0}]
+├ 🚀 Speed: {3}/s
+├ 📟 Completed: {1}/{2} 
+├ ⏳ Time: {4}
+╰─── `[✪ Team SPY ✪](https://t.me/kingofpatal)` 
+`"""
 
 async def progress_for_pyrogram(
     current,
@@ -28,6 +36,7 @@ async def progress_for_pyrogram(
                 statusMsg = json.load(f)
                 if not statusMsg["running"]:
                     bot.stop_transmission()
+
         speed = current / diff
         elapsed_time = round(diff) * 1
         time_to_completion = round((total - current) / speed) * 1
@@ -36,7 +45,7 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "**{0}{1}** \n".format(
+        progress = "{0}{1}".format(
             ''.join(                
                     FINISHED_PROGRESS_STR
                     for _ in range(math.floor(percentage / 10))                
@@ -47,7 +56,8 @@ async def progress_for_pyrogram(
             ),
         )  
 
-        tmp = progress + "**\n__Completed__:** {0} of {1}\n**__Speed__**: {2}/s\n**__Time__**: {3}\n".format(
+        tmp = PROGRESS_BAR.format(
+            progress,
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
@@ -58,7 +68,6 @@ async def progress_for_pyrogram(
             if message.text != text or message.caption != text:
                 if not message.photo:
                     await message.edit_text(text=f"{ud_type}\n {tmp}")
-
                 else:
                     await message.edit_caption(caption=f"{ud_type}\n {tmp}")
         except:
